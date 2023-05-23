bd7b6b16f92d5e0d5df9a17820a19bf54f79531e
dhananjaya weerasinghe <e18073@eng.pdn.ac.lk>
2023-05-23 17:19:02 +0530

    update aklut_tb

--- components/alu_module/alu_tb.v
+++ components/alu_module/alu_tb.v
@@ -8,13 +8,13 @@ module alu_tb;
 
     // Declare inputs
     reg [31:0] DATA1, DATA2;  // data inputs
-    reg [4:0] SELECT;         // 5 bit select control signal
+    reg [4:0] ALU_OP;         // 5 bit ALU_OP control signal
 
     // Declare output
     wire [31:0] RESULT;
 
     // Instantiate the module under test
-    alu myalu(DATA1, DATA2, RESULT, SELECT);
+    alu myalu(DATA1, DATA2, RESULT, ALU_OP);
 
     // Define task for test case
     task run_test;
@@ -27,36 +27,41 @@ module alu_tb;
 
         // Initialize inputs and reset signal
         begin
-            #5;
             DATA1 = data1;         // data 1
             DATA2 = data2;         // data 2
-            SELECT = opcode;       // opcode select
-            
-            // Define opcode to opname mapping
-            // string opname_dict[string] = '{ "00000": "ADD", "00010": "SUB", "00100": "AND", "01000": "OR", "01100": "XOR", "10000": "SLL","10100": "SRL" };
-            
-            // Set default opname for unknown opcodes
-            // opname = opname_dict[$sformatf("%05b", opcode)];
-            // if (!opname) opname = opname_dict[$sformatf("%05b", UNKNOWN_OPCODE)];
+            ALU_OP = opcode;       // opcode ALU_OP
 
-            #10;
+            #4;
             // If there an error, display the error message
             if (RESULT !== expected_result) $error($sformatf("Test case FAILED for opcode %05b", opcode));
 
 
             // else display the result
-            $display($sformatf("Test case PASSED for opcode %b", opcode));
+            $display($sformatf("Test case PASSED\nData1 = %d\nData2 = %d\nResult = %d\nExpected Result = %d", DATA1, DATA2, RESULT, expected_result));
         end
     endtask
 
     // Call task for each test case
     initial begin
+        $display("\nTest Addition operation");
         run_test(15, 5'b00000, 10, 5);
+
+        $display("\nTest Substartion operation");
         run_test(5, 5'b00010, 10, 5);
+
+        $display("\nTest Substartion operation");
         run_test(40, 5'b00100, 10, 2);
+
+        $display("\nTest Substartion operation");
         run_test(1, 5'b01000, 5, 10);
+
+        $display("\nTest Substartion operation");
         run_test(0, 5'b01100, -10, 5);
+
+        $display("\nTest Substartion operation");
         run_test(32'hFFFFFFFF, 5'b10000, 32'h0F0F0F0F, 32'hF0F0F0F0);
+
+        $display("\nTest Substartion operation");
         run_test(32'h01234567, 5'b10100, 32'h12345678, 4);
     end
 
